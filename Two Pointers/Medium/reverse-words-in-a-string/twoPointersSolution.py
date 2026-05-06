569caefe3c83b651ce5b1a9ba73464fe5bed3874
Aayush Dhakal <079bei001.aayush@sagarmatha.edu.np>
2026-05-06 09:30:49 +0545

    Solved reverse-words-in-a-string - Medium

---
+++ Two Pointers/Medium/reverse-words-in-a-string/twoPointersSolution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        
+        words = s.split()
+        left = 0
+        n=len(words)
+        right = n-1
+        while(left<right):  
+            words[left],words[right]=words[right],words[left]
+            left+=1
+            right-=1
+        return " ".join(words)
