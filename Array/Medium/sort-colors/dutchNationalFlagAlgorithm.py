93e3ffd629da2b330d849054ce124313386892d3
Aayush Dhakal <079bei001.aayush@sagarmatha.edu.np>
2026-05-10 21:48:59 +0545

    Solved sort-colors - Medium

---
+++ Array/Medium/sort-colors/dutchNationalFlagAlgorithm.py
@@ -0,0 +1,17 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n=len(nums)
+        low,mid,high = 0,0,n-1
+        while(mid<=high):
+            if nums[mid]==0:
+                nums[low],nums[mid]=nums[mid],nums[low]
+                mid+=1
+                low+=1
+            elif nums[mid]==1:
+                mid+=1
+            else:
+                nums[high],nums[mid]=nums[mid],nums[high]
+                high-=1
