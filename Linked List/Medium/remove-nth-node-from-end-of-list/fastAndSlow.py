45ad24714509ea7919dabe42028f2818126a7f4c
Aayush Dhakal <079bei001.aayush@sagarmatha.edu.np>
2026-05-16 16:12:19 +0545

    Solved remove-nth-node-from-end-of-list - Medium

---
+++ Linked List/Medium/remove-nth-node-from-end-of-list/fastAndSlow.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        slow = dummy
+        fast = dummy
+        dummy.next = head
+
+        for i in range(0,n+1):
+            fast = fast.next
+        while fast is not None:
+            fast = fast.next
+            slow = slow.next
+        slow.next = slow.next.next
+        return dummy.next
