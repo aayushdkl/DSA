0beb636accd2da9ecd27e71d433b5e02ae8d56de
Aayush Dhakal <079bei001.aayush@sagarmatha.edu.np>
2026-06-17 17:29:51 +0545

    Solved permutation-in-string - Medium

---
+++ Hash Table/Medium/permutation-in-string/slidingWindow.py
@@ -0,0 +1,23 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        n1, n2 = len(s1), len(s2)
+        if n1 > n2:
+            return False
+
+        freq1 = [0]*26
+        freq2 = [0]*26
+
+        for i in range(0,n1):
+            freq1[ord(s1[i]) - ord('a')] += 1
+            freq2[ord(s2[i]) - ord('a')] += 1
+        if freq1 == freq2:
+            return True
+        left=0
+        for right in range(n1,n2):
+            freq2[ord(s2[left])-ord('a')]-=1
+            freq2[ord(s2[right])-ord('a')]+=1
+            left+=1
+            if freq1 == freq2:
+                return True
+
+        return False
