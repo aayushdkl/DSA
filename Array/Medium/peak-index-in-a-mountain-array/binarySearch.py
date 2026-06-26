a6183152a0056dc3d60b6713a0d4379a68d753a1
Aayush Dhakal <079bei001.aayush@sagarmatha.edu.np>
2026-06-27 00:24:20 +0545

    Solved peak-index-in-a-mountain-array - Medium

---
+++ Array/Medium/peak-index-in-a-mountain-array/binarySearch.py
@@ -0,0 +1,12 @@
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        n=len(arr)
+        low,high =0,n-1
+
+        while low<high:
+            mid = low + (high-low)//2
+            if arr[mid]<arr[mid+1]:
+                low = mid+1
+            else:
+                high = mid
+        return low
