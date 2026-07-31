8680317d1d5f4c432494bd7484e966fc7fa94f29
Aayush Dhakal <079bei001.aayush@sagarmatha.edu.np>
2026-07-31 23:03:27 +0545

    Solved largest-integer-with-given-digit-sum - Easy

---
+++ Uncategorized/Easy/largest-integer-with-given-digit-sum/greedy.py
@@ -0,0 +1,14 @@
+class Solution:
+    def largestInteger(self, n: int, s: int) -> int:
+        if s == 0:
+            return 0
+        if s > 9 * n:
+            return -1
+
+        ans = []
+        for _ in range(n):
+            d = min(9, s)
+            ans.append(str(d))
+            s -= d
+
+        return int("".join(ans))
